b02148494a8f99bd45c61678ecd1988f105ecd32
samytahiri <samy.tahiri26@gmail.com>
2025-09-25 22:08:36 -0400

    ADDING FINAL theme tokyonight in moon

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,14 @@
 return {
-  "tiagovla/tokyodark.nvim",
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
   opts = {
-    transparent_background = true, -- set background to transparent
+    style = "moon",
+    transparent = true,
+
+    styles = {
+      keywords = { italic = true },
+      comments = { italic = true },
+    },
   },
-  config = function(_, opts)
-    require("tokyodark").setup(opts) -- calling setup is optional
-    vim.cmd([[colorscheme tokyodark]])
-  end,
 }
