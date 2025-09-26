66f6efe9fd10cc2c9d657fa577a6cc4988445bc2
samytahiri <samy.tahiri26@gmail.com>
2025-09-25 20:36:10 -0400

    Changing keybinds

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", ":Ex<CR>", { desc = "Open file explorer" })
 vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit file" })
-vim.keymap.set("n", "<leader>n", ":w<CR>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>s", ":w<CR>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>m", ":Mason<CR>", { desc = "Open Mason" })
 vim.keymap.set("n", "<leader>g", vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set("n", "<leader>u", "<C-o>", { desc = "Go back" })
