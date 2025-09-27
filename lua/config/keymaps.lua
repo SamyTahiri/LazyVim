c0bc5799f6bcb7abfa62b56ef9c67114c86aa86f
samytahiri <samy.tahiri26@gmail.com>
2025-09-26 22:22:21 -0400

    fixing run keybind

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,4 +6,9 @@ vim.keymap.set("n", "<leader>m", ":Mason<CR>", { desc = "Open Mason" })
 vim.keymap.set("n", "<leader>g", vim.lsp.buf.definition, { desc = "Go to definition" })
 vim.keymap.set("n", "<leader>u", "<C-o>", { desc = "Go back" })
 vim.keymap.set("n", "<leader>r", ":!javac % && :!java %", { desc = "Go back" })
-vim.keymap.set("n", "<leader>r", ":!javac % && java %:r<CR>", { desc = "Compile & run current Java file" })
+
+vim.keymap.set("n", "<leader>r", function()
+  local file = vim.fn.expand("%") -- current file
+  local name = vim.fn.expand("%:r") -- filename without extension
+  vim.cmd("split | terminal javac " .. file .. " && java " .. name)
+end, { desc = "Compile & run current Java file in terminal" })
